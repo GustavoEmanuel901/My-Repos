3112e1e9f47aee5d9f7e8f697c23d667f984ca5c
Gustavo Emanuel <gustavoemanuel901@gmail.com>
2020-08-13 13:17:40 -0300

    key correction

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -6,14 +6,14 @@ import './styles.css'
 
 const ListItem = ({name, id, favorite, handleFavorite}) => {
   return (
-        <li key={id}>
-        <a href={`https://github.com/GustavoEmanuel901/${name}`} target="_blanck">{name}</a>
-        {favorite && <span>(Favorito)</span>}
+        <li>
+            <a href={`https://github.com/GustavoEmanuel901/${name}`} target="_blanck">{name}</a>
+            {favorite && <span>(Favorito)</span>}
             <button onClick={() => handleFavorite(id)}>
                 <FiStar/> 
                 Favoritar
             </button>
-    </li>        
+        </li>        
   );
 }
 
